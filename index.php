38944adb5b2b344f85fac442687c318be823f8f8
rowanweathers <74547960+rowanweathers@users.noreply.github.com>
2020-12-11 16:47:12 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+Plugin Name: RowanSays: Reposition Kint
+Description: Allows for better display of kint debug box in wp-admin/.
+Version: 1.0.0
+Requires PHP: 7.4
+Author: Rowan Weathers
+Author URI: https://rowansays.com/
+
+License: GPL-3.0-or-later
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace RowanSays\Wp\Plugin\RepositionKint;
+
+if (WP_DEBUG === true) {
+  add_action('admin_enqueue_scripts', function () {
+    $file = 'style.css';
+    $path = plugin_dir_path(__FILE__) . $file;
+    $url = plugin_dir_url(__FILE__) . $file;
+    wp_enqueue_style('RowanSayRepositionKint', $url, [], filemtime($path));
+  });
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Reposition Kint
+
+[Kint](https://github.com/kint-php/kint) is a great tool for debugging in [PHP](https://www.php.net/). However, a problem arises when using Kint in the wp-admin/ section of WordPress. The WordPress admin menu often renders over the information displayed by the Kint boxen. This simple plugin provides the Kint boxen with the appropriate margins to avoid being obscured.
